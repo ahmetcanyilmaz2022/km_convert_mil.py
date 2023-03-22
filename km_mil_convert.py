306ac59506fb18c7cac153f5e6e1f134e5d95b9d
ahmetcanyilmaz2022 <121444420+ahmetcanyilmaz2022@users.noreply.github.com>
2023-03-22 13:50:28 +0300

    first commit

---
+++ km_mil_convert.py
@@ -0,0 +1,17 @@
+
+
+
+km = float(input("Enter Km: ")) #Let's enter the first desired value #1st process
+
+
+ratio = 0.621371 #km--mil ratio   #2nd process
+
+mil = km * ratio    #3th  process
+
+print('%0.2f kilometers is %0.2f miles' %(km,mil)) #finishing
+
+
+#a simple beginner example Acan Yılmaz
+
+
+
